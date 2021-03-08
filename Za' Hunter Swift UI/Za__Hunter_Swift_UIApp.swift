acb28e133794c7e6f4aba739c9e673984dc61e8c
Avery Zakson <avery.zakson@gmail.com>
2021-03-08 10:28:28 -0600

    Initial Commit

---
+++ Za' Hunter Swift UI/Za__Hunter_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za__Hunter_Swift_UIApp.swift
+//  Za' Hunter Swift UI
+//
+//  Created by Avery Zakson on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Za__Hunter_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
